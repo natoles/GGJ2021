9b8435c92b36c0175490f0b100c55d9d6ebc966d
Baptiste ROSEAU <broseau@gmail.com>
2021-01-30 00:11:46 +0100

    Added Enclosure Code, Prefab and Tag

--- Assets/Script/Enclosure.cs
+++ Assets/Script/Enclosure.cs
@@ -6,15 +6,83 @@ public class Enclosure : MonoBehaviour
 {
     // PUBLIC
     public List<Animal> animals;
-
+    public int totalSpace = 10;
+    public int currentUsedSpace = 0;
 
     // PRIVATE
-    private bool isFull = false;
+
+
+    // METHODS
+
+    public static Vector2 RandomPointInBounds(Bounds bounds)
+    {
+        return new Vector2(
+            Random.Range(bounds.min.x, bounds.max.x),
+            Random.Range(bounds.min.y, bounds.max.y)
+            );
+    }
+
+    // Returns a random point into the collider
+    public Vector2 RandomPoint()
+    {
+        return RandomPointInBounds(gameObject.GetComponent<Collider>().bounds);
+    }
+
+    public bool IsFull()
+    {
+       return currentUsedSpace >= totalSpace;
+    }
+
+    public bool IsEmpty()
+    {
+       return currentUsedSpace <= 0;
+    }
+
+    // Removes all animals from the enclosure
+    public void Empty()
+    {
+       currentUsedSpace = 0;
+       animals.Clear();
+    }
+
+    // Add an animal into the enclosure. Return 0 for success and -1 on fail.
+    public int AddAnimal(Animal animal)
+    {
+        if (IsFull()) return -1;
+        
+        animals.Add(animal);
+        currentUsedSpace += 1;
+
+        return 0;
+    }
+
+    // Remove an animal into the enclosure. Return 0 for success and -1 on fail.
+    public int RemoveAnimal(Animal animal)
+    {
+        if (IsEmpty()) return -1;
+        
+        animals.Remove(animal);
+        currentUsedSpace -= 1;
+
+        return 0;
+    }
+
+    // Remove an animal into the enclosure. Return 0 for success and -1 on fail.
+    public int TransferAnimal(Animal animal, Enclosure enclosure)
+    {
+        if (enclosure.IsFull()) return -1;
+        if (!animals.Contains(animal)) return -1;
+
+        if (enclosure.AddAnimal(animal) != 0) return -1;
+        if (RemoveAnimal(animal) != 0) return -1;
+        
+        return 0;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        isFull = false;
+        Empty();
     }
 
     // Update is called once per frame
