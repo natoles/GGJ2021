594edf400666002e39b24838fda9ae118991b6e7
Baptiste ROSEAU <broseau@gmail.com>
2021-01-30 16:54:37 +0100

    Added GameManager and StringToClass

---
+++ Assets/Script/MyGameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class 
+public class GameManager : MonoBehaviour
+{
+
+    // TODO:
+    /*
+    - Gestion du spawn des animaux
+    - Level manager -> prends un tableau en entrée
+    - Référence à chacun des animaux/enclos/règles
+    - Boucle de jeu
+    - Barre de stress
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
