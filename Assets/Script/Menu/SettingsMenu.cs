2f5e0a0b997bced7f797003152ae02a6b14e2a45
alvina-dr <alvinadr24@gmail.com>
2021-01-31 00:37:20 +0100

    menu + bataille sprite

---
+++ Assets/Script/Menu/SettingsMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+    public Dropdown ResolutionDropdown;
+
+    private void Start()
+    {
+        int CurrentResolutionIndex = 0;
+        resolutions = Screen.resolutions;
+
+        ResolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string Option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(Option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                CurrentResolutionIndex = i;
+            }
+        }
+
+        ResolutionDropdown.AddOptions(options);
+        ResolutionDropdown.value = CurrentResolutionIndex;
+        ResolutionDropdown.RefreshShownValue();
+    }
+    public void SetResolution(int ResolutionIndex)
+    {
+        Resolution resolution = resolutions[ResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+    public void SetFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
