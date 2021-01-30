646d85212560963d3e22d84d4e5aa5857538e589
natoles <mart.anatole@gmail.com>
2021-01-30 17:12:44 +0100

    Mouse
    ZAWARUDO

---
+++ Assets/Script/Animals/Mouse.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : Animal
+{
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+
+        minMoveInterval = 1f;
+        maxMoveInterval = 2f;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override IEnumerator RageState()
+    {
+        while (true)
+        {
+            //Put rage behavior here
+            yield return new WaitForSeconds(Random.Range(minMoveInterval, maxMoveInterval));
+        }
+
+    }
+}
