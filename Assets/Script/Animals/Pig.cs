d4388924b873b1d825a77effc55016c0a5379892
alvina-dr <alvinadr24@gmail.com>
2021-01-31 15:21:48 +0100

    rép merge script pig

--- Assets/Script/Animals/Pig.cs
+++ Assets/Script/Animals/Pig.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Pig : Animal
 {
-<<<<<<< HEAD
-=======
 
     public void ThrowMud()
     {
         
     }
-
->>>>>>> f85119925ad560a42c98d3725d4c60e1ef9fa42c
     // Start is called before the first frame update
     protected override void Start()
     {
